44e7c05f64b519b793fd8c92ef9f4d442c448c1b
zntb <zenetibi@yahoo.com>
2024-01-07 07:22:47 +0200

    Optimistic Updating the Event Details

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -5,7 +5,7 @@ import { useMutation } from '@tanstack/react-query';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
@@ -14,6 +14,24 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ['events', params.id] });
+      const previousEvent = queryClient.getQueryData(['events', params.id]);
+
+      queryClient.setQueryData(['events', params.id], newEvent);
+
+      return { previousEvent: previousEvent };
+    },
+
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent);
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]);
+    },
   });
 
   const { data, isPending, isError, error } = useQuery({
